d99d30e65bf873007b5a761b8b898234c261a693
Programmer Ammo <100178397+EternalBlue3@users.noreply.github.com>
2022-03-28 18:13:24 -0400

    Create quizlet_match.js

---
+++ quizlet_match.js
@@ -0,0 +1,22 @@
+function getClass(id) {
+	return document.getElementsByClassName(id);
+}
+
+function match() {
+button = getClass("UIButton UIButton--hero")[0].click();
+setTimeout(function() {
+var matchLoop = setInterval(function() {
+waitForMatch();
+}, 0);
+				
+function waitForMatch() {
+if (getClass("MatchModeQuestionScatterTile") || getClass("MatchModeQuestionGridBoard-tile")) {
+for (var F = setTimeout(";"), i = 0; i < F; i++) clearTimeout(i);
+var tiles = getClass("MatchModeQuestionScatterTile");
+						clearTimeout(matchLoop);
+					}
+				}
+			}, 500);
+		}
+
+match();
